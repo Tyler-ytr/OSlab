e96719df09750dded41c8702cd3bc0799776d392
tracer-nju <tracer@nju.edu.cn>
2019-02-26 15:19:19 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 33eae937ea3b4747bf74f2a858c0ce28   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  15:19:19 up  5:13,  5 users,  load average: 0.79, 0.83, 1.14

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
 #include <dirent.h>
+
 //I use stat to get the information the the documents;
 //I use scandir to read the catalog
+
+void test()
+{
+    struct dirent **namelist;
+    int n;
+    n=scandir(".",&namelist,0,alphasort);
+    if(n<0)
+    {
+        perror("scandir");
+    }
+    else
+    {
+        while(n--)
+        {
+            printf("%s/n",namelist[n]->d_name);
+            free(namelist[n]);
+        }
+        free(namelist);
+    }
+}
+
+
+
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
   int i;
@@ -13,5 +37,13 @@ int main(int argc, char *argv[]) {
   printf("I am here\n");
   assert(!argv[argc]); // always true
   printf("I am here\n");
+  test();
   return 0;
 }
+
+
+
+
+
+
+
