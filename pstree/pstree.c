658782ff12dcaac7ecb268ccacfb52c676372d11
tracer-nju <tracer@nju.edu.cn>
2019-02-26 19:35:53 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 33eae937ea3b4747bf74f2a858c0ce28   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  19:35:53 up  9:30,  8 users,  load average: 0.48, 0.73, 0.86

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]) {
   }
 
   
-  for(int i=0;i<total;++i)
+  for(int i=0;i<2;++i)
   {
     ;//printf("%s\n",namelist[i]->d_name);
     strcpy(temp_proc_path,charproc);
