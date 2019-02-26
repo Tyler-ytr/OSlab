62794f5249bcf5b1dd50fd553c0ab74b3dcd2355
tracer-nju <tracer@nju.edu.cn>
2019-02-26 15:57:25 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 33eae937ea3b4747bf74f2a858c0ce28   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  15:57:25 up  5:51,  5 users,  load average: 0.36, 0.46, 0.67

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -35,7 +35,7 @@ int choose_num(const struct dirent *dir)
    for(int i=0;i<n;++i)
    {
        if(!isdigit(dir->d_name[i]))
-       {   printf("%c\n",dir->d_name[i]);
+       {   //printf("%c\n",dir->d_name[i]);
            return 0;
        }
        else return 1;
