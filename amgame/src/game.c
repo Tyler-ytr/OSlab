a1e2c21c0ebfa8d4a54e41b476be69cd3041c707
tracer-nju <tracer@nju.edu.cn>
2019-03-05 21:03:38 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: d60c3d26222c426b8dc1fc40ee18a165   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  21:03:38 up  2:07,  7 users,  load average: 0.73, 0.78, 0.87

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,24 +1,24 @@
 #include <game.h>
+#include<klib.h>
 
 void init_screen();
 void splash();
-char read_key();
+void read_key2();
 
 int main() {
   // Operating system is a C program
   _ioe_init();
   init_screen();
   splash();
-  char key;
+  //int key;
   while (1) {
-    key=read_key();
-
-    puts(&key);
+    read_key();
+  //  puts(&key);
   }
   return 0;
 }
 
-char read_key() {
+void read_key2() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
@@ -31,7 +31,7 @@ char read_key() {
     puts(key_names[event.keycode]);
     puts("\n");
   }
-  return _KEY_NONE;
+  //return event.keycode;
 }
 
 int w, h;
@@ -43,7 +43,7 @@ void init_screen() {
   h = info.height;
 }
 
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
+void draw_rect2(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: allocated on stack
   _DEV_VIDEO_FBCTL_t event = {
     .x = x, .y = y, .w = w, .h = h, .sync = 1,
@@ -59,7 +59,7 @@ void splash() {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
-        draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
+        draw_rect2(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
       }
     }
   }
