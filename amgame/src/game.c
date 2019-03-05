ef99971c5b7ee22350bcc95db511bd9601f0cb28
tracer-nju <tracer@nju.edu.cn>
2019-03-05 21:35:21 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: d60c3d26222c426b8dc1fc40ee18a165   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  21:35:21 up  2:39,  7 users,  load average: 0.94, 0.97, 0.98

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -17,8 +17,8 @@ int main() {
   while (1) {
     key = read_key2();
     if(key!=0)printf("%d\n",key);
-    for(int x=w/2-10;x*SIDE<=w/2+10;x++)
-    for(int y=w/2-10;y*SIDE<=w/2+10;y++)
+    for(int x=w/2-10*SIDE;x*SIDE<=w/2+10;x++)
+    for(int y=w/2-10*SIDE;y*SIDE<=w/2+10;y++)
     {
 
         draw_rect2(x * SIDE, y * SIDE, SIDE, SIDE, 0x191970); // white
