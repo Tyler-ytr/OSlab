b369f29101d9d26b0540a6f05f7852f367a7f569
tracer-nju <tracer@nju.edu.cn>
2019-03-06 17:11:09 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 978bc9052dd7427cb7013bfc8dfe87b6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  17:11:09 up  1:08,  8 users,  load average: 0.44, 0.55, 0.48

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -205,15 +205,15 @@ int read_key2() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
-  static const char *key_names[] = {
-    _KEYS(KEYNAME)
-  };
+  //static const char *key_names[] = {
+   // _KEYS(KEYNAME)
+  //};
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
-  if (event.keycode != _KEY_NONE && event.keydown) {
-    puts("Key pressed: ");
-    puts(key_names[event.keycode]);
-    puts("\n");
-  }
+  //if (event.keycode != _KEY_NONE && event.keydown) {
+   // puts("Key pressed: ");
+   // puts(key_names[event.keycode]);
+   // puts("\n");
+ // }
   return event.keycode;
 }
 
