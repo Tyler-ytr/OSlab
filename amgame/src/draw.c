02496d95e69618effbb27412b531edd23c0ffc60
tracer-nju <tracer@nju.edu.cn>
2019-03-06 16:41:21 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 978bc9052dd7427cb7013bfc8dfe87b6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  16:41:21 up 39 min,  7 users,  load average: 0.23, 0.29, 0.24

--- amgame/src/draw.c
+++ amgame/src/draw.c
@@ -12,7 +12,7 @@ void draw_character(char ch, int x, int y,int color)
         {
             if((p[i]>>j)&1)
             {
-                 draw_rect2(i,j,10,10, color);
+                 draw_rect2(i*16,j*16,2,2, color);
                 
             }
         }
