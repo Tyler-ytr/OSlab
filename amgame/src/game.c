3709319fa0543971b421cba2a460e1c4a8534ef4
tracer-nju <tracer@nju.edu.cn>
2019-03-06 16:52:24 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 978bc9052dd7427cb7013bfc8dfe87b6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  16:52:24 up 50 min,  8 users,  load average: 0.37, 0.34, 0.29

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -66,7 +66,8 @@ printf("x: %d y: %d \n",food_x,food_y);
         splash();
   while(1)
   {
-       draw_character('G',cnt_w/2,cnt_h/2,_red);
+       draw_character('G',cnt_w/2-5,cnt_h/2-5,_red);
+       draw_character('G',cnt_w/2-5,cnt_h/2-5,_red);
 
   }
   printf("GG");
