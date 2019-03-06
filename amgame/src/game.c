70a6226c39e0105b38fa890f3e14f36d938e2fd9
tracer-nju <tracer@nju.edu.cn>
2019-03-06 14:13:56 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 71e89dceefed49bfb72fce8d54dbd37d   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  14:13:56 up  1:05,  6 users,  load average: 1.70, 1.38, 0.93

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -113,6 +113,15 @@ void _snakemove(int key)
     snake[1]._snake_y+=direction_y;
     
 
+    //判断有没有吃到食物
+
+    if(snake[1]._snake_x==food_x&&snake[1]._snake_y==food_y)
+    {
+        eat=1;
+    }
+
+
+
 }
 void _snakedraw()
 {
