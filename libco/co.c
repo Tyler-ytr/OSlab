1adaea13de10bf9816837ef43bc96694e078df92
tracer-nju <tracer@nju.edu.cn>
2019-03-16 11:56:34 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 06d8b10481494959a62ecdbcd71624bc   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  11:56:35 up 4 min,  3 users,  load average: 0.99, 1.24, 0.62

--- libco/co.c
+++ libco/co.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <ucontext.h>
+#include <unistd.h>
 #include "co.h"
+//借助了ucontext库,主要使用int getcontext(ucontext_t *ucp);int setcontext(const ucontext_t *ucp);void makecontext(ucontext_t *ucp, void (*func)(), int argc, ...);int swapcontext(ucontext_t *oucp, ucontext_t *ucp);四个库中的函数来实现协程
+//
+//使用协程的程序会首先调用co_init完成一些必要的初始化。如果你的实现并不需要在启动时做任何初始化，你可以留下一个空的函数。
 
 struct co {
 };
+//co_start创建一个新的协程，并返回一个指针(动态分配内存)。我们的框架代码中并没有限定 struct co 结构体的设计，所以你可以自由发挥
 
 void co_init() {
 }
@@ -12,9 +18,11 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   return NULL;
 }
 
+//co_yield 是指当前运行的协程放弃执行，并切换到其他协程执行。系统中可能有多个运行的协程(包括当前协程)。你可以随机选择下一个系统中可运行的协程
 void co_yield() {
 }
 
+//co_wait(thd) 表示当前协程不再执行，直到 thd 协程的执行完成。我们规定，每个协程的资源在co_wait()等待结束后释放，因此每个协程只能被co_wait一次。更精确地说，每个协程必须恰好被co_wait一次，否则就会造成资源泄露。
 void co_wait(struct co *thd) {
 }
 
