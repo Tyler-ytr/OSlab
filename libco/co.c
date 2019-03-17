932724677b10f2fc7acba7699690ffe8f3c29122
tracer-nju <tracer@nju.edu.cn>
2019-03-17 15:58:38 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 29ce14258cd74337b7f3d14e3b746dfb   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  15:58:38 up  5:12,  5 users,  load average: 0.36, 0.62, 0.63

--- libco/co.c
+++ libco/co.c
@@ -68,7 +68,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 
   coroutines[_TOTAL].ctx.uc_stack.ss_flags=0;
   _NOW=_TOTAL;
-  //printf("here\n");
+  printf("here before make\n");
   makecontext(&(coroutines[_TOTAL].ctx),(void (*)(void))thread_body,1);
   //printf("hhere\n");
 
