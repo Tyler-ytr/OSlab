3fd210edd8ae77e6691b15898dd242ccacb99dc3
tracer-nju <tracer@nju.edu.cn>
2019-03-26 18:03:33 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 434087f6f39e4ce789b3372f36b13183   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  18:03:33 up  3:32,  6 users,  load average: 0.45, 0.53, 0.52

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -7,8 +7,8 @@ typedef struct pThread_t
   intptr_t lock;
 }pthread_t;
 
-extern intptr_t my_spin_lock(struct pthread_t  *locked);
-extern intptr_t my_spin_unlock(intptr_t locked); 
+extern intptr_t my_spin_lock( pthread_t  *locked);
+extern intptr_t my_spin_unlock(pthread_t *locked); 
 
 //#define pthread_t intptr_t
 
