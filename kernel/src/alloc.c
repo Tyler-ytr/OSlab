0a5575ac8807daaded48719f462bab1285735040
tracer-nju <tracer@nju.edu.cn>
2019-04-02 14:32:55 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: ff10804355814ecbab631610b93a5a13   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  14:32:55 up 49 min,  4 users,  load average: 0.25, 0.62, 0.78

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -14,8 +14,9 @@ static void *kalloc(size_t size) {
  my_spin_lock(alloc_lock);
  start+=size;
  void *ret=&start; 
+ int result=*ret;
  my_spin_unlock(alloc_lock);
- printf("%d\n",(*ret));
+ printf("%d\n",result);
   
   return ret;
 }
