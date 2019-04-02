2eff00b7e1bc9ec72a0ec171d3ba857457473994
tracer-nju <tracer@nju.edu.cn>
2019-04-02 19:46:46 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: ff10804355814ecbab631610b93a5a13   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  19:46:46 up  6:02,  5 users,  load average: 0.59, 0.55, 0.50

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -25,7 +25,7 @@ static void pmm_init() {
 
   bound*b1=max;
   b1[0].left_bound=&b1[1];
-  printf("\nb1:0x%x,b1[0]:0x%x,b1[0].left_bound:0x%x;b1[1]:0x%x\n",&b1,&b1[0],&b1[0].left_bound,&b1[1]);
+  printf("\nb1:0x%x,b1[0]:0x%x,b1[0].left_bound:0x%x;b1[1]:0x%x\n",&b1,&b1[0],b1[0].left_bound,&b1[1]);
 
 
   printf("head_place:0x%x,head->next：0x%x",&head,head->next);
