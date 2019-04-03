15ec1b1cfe3c0c24cec4ae47cf4142eb0ab06397
tracer-nju <tracer@nju.edu.cn>
2019-04-03 11:03:10 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: ec6ee3d27c7b4c9884cda0146231ab41   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  11:03:10 up 24 min,  1 user,  load average: 1.47, 1.42, 1.16

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -4,8 +4,8 @@
 static uintptr_t pm_start, pm_end;
 //static uintptr_t start;
 static void pmm_init() {
-  pthread_t init_lock=0;
-  my_spin_lock(init_lock);
+  //pthread_t init_lock=0;
+  //my_spin_lock(init_lock);
   pm_start = (uintptr_t)_heap.start;
   printf("start:0x%x",pm_start);
   pm_end   = (uintptr_t)_heap.end;
@@ -31,7 +31,7 @@ static void pmm_init() {
   printf("result:0x%x bound_area:0x%x\n",result,sizeof(bound));*/
   printf("head_place:0x%x,head->next：0x%x,head->addr:0x%x\n",&head[0],head->next,head[0].addr);
  // printf("test_place:0x%x,test.next：0x%x",&test,test.next);
-  my_spin_unlock(init_lock);
+//  my_spin_unlock(init_lock);
 }
 
 static void *kalloc(size_t size) {
