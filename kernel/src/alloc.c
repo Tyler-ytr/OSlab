6ea33f3477a59e46bef22cc78d898fbd13c7e550
tracer-nju <tracer@nju.edu.cn>
2019-04-04 00:05:55 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: d2a3548c5fde40b8b47df67ae1b3c44a   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  00:05:55 up 48 min,  2 users,  load average: 0.79, 0.67, 0.81

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -158,7 +158,7 @@ static void *kalloc(size_t size) {
       assert(now->next->prev==now);
 
       if(new->next->prev!=new){
-        printf("new: 0x%x  -----: 0x%x\n",new,new->next);
+        printf("new: 0x%x  -----: 0x%x  size:%d\n",new,new->next,size);
         assert(0);
       }
       assert(now->prev->next==now);
