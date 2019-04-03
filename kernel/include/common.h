4a30c7ec269f508f25f7c80aae5ca975ee988094
tracer-nju <tracer@nju.edu.cn>
2019-04-03 10:58:35 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: ec6ee3d27c7b4c9884cda0146231ab41   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  10:58:35 up 20 min,  1 user,  load average: 1.93, 1.46, 1.09

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -81,7 +81,7 @@ xchg(volatile uint *addr, uint newval)
   return result;
 }
 struct cpu {
-  unsigend char  apicid;                // Local APIC ID
+  unsigned char  apicid;                // Local APIC ID
   struct context *scheduler;   // swtch() here to enter scheduler
   //struct taskstate ts;         // Used by x86 to find stack for interrupt
 //  struct segdesc gdt[NSEGS];   // x86 global descriptor table
