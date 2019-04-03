c933942169d10710d1bc0acef4a1cd9219f7d7e7
tracer-nju <tracer@nju.edu.cn>
2019-04-03 13:42:12 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 2c56cebdbd1b46419359393f92c457f0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  13:42:12 up 47 min,  1 user,  load average: 1.70, 1.55, 1.27

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -39,14 +39,8 @@ static void pmm_init() {
   unused_space->size=0;
 
   cpu_head[0]=&unused_space[1];
-  int i=1;
-  for(i=1;i<=5;i++)
-  {
-    cpu_head[i]=&(cpu_head[0])[i];
-    printf("cpu : %d 0x%x",i,cpu_head[i]);
-  }
-  //cpu_head[2]=&(cpu_head[0])[2];
-
+  cpu_head[1]=&(cpu_head[0])[1];
+  cpu_head[2]=&(cpu_head[0])[2];
   printf("cpu_area: 0x%x, 1: 0x%x ; 2: 0x%x \n",cpu_head[0],cpu_head[1],&cpu_head[2]);
 
 
