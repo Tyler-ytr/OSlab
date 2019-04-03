237b0f61622ac13023a5f163c689648fc70eba26
tracer-nju <tracer@nju.edu.cn>
2019-04-03 10:56:22 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: ec6ee3d27c7b4c9884cda0146231ab41   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  10:56:23 up 17 min,  1 user,  load average: 0.91, 1.14, 0.94

--- kernel/src/lock.c
+++ kernel/src/lock.c
@@ -9,13 +9,14 @@
 //#include "proc.h"
 //#include "spinlock.h"
 
+/*
 intptr_t xchg(volatile intptr_t *addr, intptr_t newval) {
   intptr_t result;
 asm volatile ("lock xchgl %0, %1":
     "+m"(*addr), "=a"(result) : "1"(newval) : "cc");
 return result; 
 }
-
+*/
 /*
 intptr_t my_spin_lock(intptr_t locked)
 {
