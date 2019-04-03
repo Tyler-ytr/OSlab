48d19a419b3523b47fa53fdcb4849b39bdc3cd24
tracer-nju <tracer@nju.edu.cn>
2019-04-03 23:59:19 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: d2a3548c5fde40b8b47df67ae1b3c44a   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  23:59:19 up 41 min,  2 users,  load average: 0.68, 0.93, 0.96

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -134,7 +134,7 @@ static void *kalloc(size_t size) {
   }
   else
   {//下面的操作是拆分或者直接使用,所以不用修改unused_space；
-    if((int)(now->size-size-sizeof(_node))>0)
+    if((int)(now->size-size-2*sizeof(_node))>0)
     {
       assert((int)(now->size-size)>sizeof(_node));
       //返回的是now的地址;
