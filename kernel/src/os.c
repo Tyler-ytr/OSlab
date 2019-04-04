7e935e92a55039da9ab1f00795879737e88867e2
tracer-nju <tracer@nju.edu.cn>
2019-04-04 14:42:05 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 340bbcd4c28142f18a2250dbf5c7e5f1   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  14:42:05 up 32 min,  1 user,  load average: 1.27, 1.28, 1.23

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -3,9 +3,9 @@
 spinlock os_lk;
 
 void test_from_yzy(){
-      void *space[1000];
+      void *space[500];
           int i;
-          for(i=0;i<1000;++i){
+          for(i=0;i<500;++i){
                     space[i]=pmm->alloc(rand()%((1<<10)-1));
                     printf("i: %d space:0x%x\n",i,space[i]);
                         
