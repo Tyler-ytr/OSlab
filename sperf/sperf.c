06de1e35d45e36ee7819ac57c58da52f83d42aaa
tracer-nju <tracer@nju.edu.cn>
2019-04-11 22:06:03 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 86d169c46d6743d2836dacc8b1a417ad   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:06:03 up  1:53,  5 users,  load average: 1.22, 1.02, 1.03

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,6 +1,19 @@
+//Makefile 主要参考了libco/tests中的Makefile;make run 是 64 与 32 bit的test,另外还有make run32 ; make run64;
 #include <stdio.h>
+
+#include <unistd.h>
+void test();
 int main(int argc, char *argv[]) {
   
-  printf("Hello world!!!\n");
+  printf("Hello world!!\n");
+
+  test(argv);
+  //strace -T ls
   return 0;
 }
+void test(char *argv[]){
+
+char *argva[]={"strace","-T",argv[1],NULL};//传递给执行文件的参数数组，这里包含执行文件的参数 
+
+  char *envp[]={0,NULL};//传递给执行文件新的环境变量数组
+  execve("/usr/bin/strace",argva,envp);}
