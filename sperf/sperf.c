81efa397a5dd2bed431f39164bd69e03f3d0417a
tracer-nju <tracer@nju.edu.cn>
2019-04-11 22:38:37 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 86d169c46d6743d2836dacc8b1a417ad   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:38:37 up  2:25,  5 users,  load average: 1.41, 1.53, 1.27

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,7 +3,8 @@
 
 #include <unistd.h>
 #define Log(format, ...) \
-    printf("\33[1;35m[%s,%d,%s] " format "\33[0m\n", \"]]")
+    printk("\33[1;35m[%s,%d,%s] " format "\33[0m\n", \
+              __FILE__, __LINE__, __func__, ## __VA_ARGS__)"]]")
 void test();
 int main(int argc, char *argv[],char *envp[]) {
   
