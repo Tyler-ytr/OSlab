06f508f67b7c576dc23991ab52205970c1a92682
tracer-nju <tracer@nju.edu.cn>
2019-04-11 22:16:30 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 86d169c46d6743d2836dacc8b1a417ad   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:16:30 up  2:03,  5 users,  load average: 0.89, 1.02, 1.02

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -11,6 +11,10 @@ int main(int argc, char *argv[]) {
   {
     printf("argv : %s\n",argv[i]);
   }
+char *argva[]={"/usr/bin/strace","-T",argv[1],">/dev/null",NULL};//传递给执行文件的参数数组，这里包含执行文件的参数 
+
+  char *envp[]={0,NULL};//传递给执行文件新的环境变量数组
+  execve("/usr/bin/strace",argva,envp);
 //  test(argv);
   //strace -T ls
   return 0;
