941ed0926bfae9abd7abeef63681da1d982d47c0
tracer-nju <tracer@nju.edu.cn>
2019-04-12 23:40:19 +0800

    Static hostname: larryytr-ThinkPad          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8a6797b85fe64dd3a95f0aab71ff3d27            Boot ID: 7a06d246d6974c098d856fa8331329ed   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  23:40:20 up  1:02,  3 users,  load average: 1.31, 0.68, 0.44

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -107,7 +107,7 @@ char *argva[]={"strace","-T",argv[1],NULL};//传递给执行文件的参数数�
     for(int i=0;i<G.num;i++)
     {
       //printf("%s: %lf %lf%%\n",funinfo[i].func_name,funinfo[i].func_time,funinfo[i].func_time/G.total_time*100);
-      printf("%s:%20lf%%\n",funinfo[i].func_name,funinfo[i].func_time/G.total_time*100);
+      printf("%10s:%20lf%%\n",funinfo[i].func_name,funinfo[i].func_time/G.total_time*100);
     }
     printf("\n\n\n\n");
 
