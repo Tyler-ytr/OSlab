9c81b89dffdf78ff104f61e39d1fdca47ec578a1
tracer-nju <tracer@nju.edu.cn>
2019-05-03 14:40:36 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 3c66198c8fab458d991e475bb755e7dd   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  14:40:36 up  5:28,  3 users,  load average: 0.77, 0.88, 0.86

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Somethingwrong(char *str)
+{
+    perror(str);
+
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[]) {
-  return 0;
+    if(system("mkdir test")!=0)
+        Somethingwrong("mkdir test");
+    printf("Success");
+
+
+    return 0;
 }
