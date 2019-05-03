c571c3dba1f9325b664961390ee2ad3817d8738e
tracer-nju <tracer@nju.edu.cn>
2019-05-03 15:12:24 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 3c66198c8fab458d991e475bb755e7dd   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  15:12:24 up  6:00,  3 users,  load average: 0.68, 0.91, 0.95

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#define MAX_LEN 512
+
 void Exitcrepl();
 void Somethingwrong(char *str)
 {
@@ -25,12 +28,32 @@ void Exitcrepl()
 
 }
 
+char* my_read(const char *previous,char * buf){
+    printf("%s ",previous);
+    char *result=NULL;
+    result=fgets(buf,MAX_LEN,stdin);
+    if(ferror(stdin))
+    {
+        Somethingwrong("my_read");
+    }
+    len=strlen(buf);
+    buf[len-1]='\0';
+    return result;
+
+}
+
 int main(int argc, char *argv[]) {
+    //创建测试目录以及lib目录;
     if(system("mkdir test")!=0)
         Somethingwrong("mkdir test");
     if(system("mkdir lib")!=0)
         Somethingwrong("mkdir lib");
-
+    //输入部分;
+    char in[MAX_LEN];
+    while(my_read(">>",in)!=NULL)
+    {
+        printf("%s\n",in);
+    }
 
     Exitcrepl();
     printf("Success");
