3ad7fd03824f3de4dab37cf1cae968eb299cc092
tracer-nju <tracer@nju.edu.cn>
2019-05-09 14:36:57 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: e65c16addfc94c83955a1960abd54031   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  14:36:57 up 38 min,  8 users,  load average: 0.93, 1.06, 1.01

---
+++ kernel/src/kmt.c
@@ -0,0 +1,64 @@
+#include <common.h>
+
+
+static void kmt_init(){
+
+    //TO BE DONE
+    return;
+}
+
+static int kmt_create((task_t *task, const char *name, void (*entry)(void *arg), void *arg){
+
+    //TO BE DONE
+
+
+    return NULL;
+}
+
+static void kmt_teardown(task_t *task){
+
+    //TO BE DONE
+
+    return;
+}
+
+static void kmt_spin_init(spinlock_t *lk, const char *name){
+
+
+    //TO BE DONE
+    return ;
+}
+
+
+static void kmt_spin_lock(spinlock_t *lk){
+
+    //TO BE DONE
+    return ;
+}
+
+
+static void kmt_spin_unlock(spinlock_t *lk){
+
+
+
+
+    //TO BE DONE
+    return ;    
+}
+static void kmt_sem_init(sem_t *sem, const char *name, int value){
+
+
+    return ;
+}
+
+static void kmt_sem_wait(sem_t *sem){
+
+
+    return;
+}
+
+void kmt_sem_signal(sem_t *sem){
+
+
+    return;
+}
