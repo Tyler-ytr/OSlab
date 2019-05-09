255d506982c4cb1f61501b4cf619f380cf7729a3
tracer-nju <tracer@nju.edu.cn>
2019-05-09 22:01:50 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 0b8729bc01b245c18dd420e6b69f4a29   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  22:01:50 up  1:36,  5 users,  load average: 2.77, 2.17, 1.77

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -53,6 +53,7 @@ popcli(void)
   if(get_efl()&FL_IF)//readflags==get_efl
     panic("popcli - interruptible");
   if(--ncli[(int)_cpu()] < 0)
+    printf("cpu: %d %d: ",(int)_cpu(),ncli[(int)_cpu()])
     panic("popcli");
  // if(--ncli[(int)_cpu()] == 0 && mycpu()->intena)
  if(--ncli[(int)_cpu()] == 0&&intena[(int)_cpu()])
@@ -84,7 +85,7 @@ holding(struct spinlock *lock)
   int r;
   pushcli();
   r = lock->locked && lock->cpu == (int)_cpu();
-  popcli();
+ popcli();
   return r;
 }
 
