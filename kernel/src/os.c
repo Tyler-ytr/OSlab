46b607f2dc4c1a7eda73da941576434d1999a628
tracer-nju <tracer@nju.edu.cn>
2019-05-11 21:59:48 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 991e582a519a4bfe96e3a09ef409d730   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  21:59:48 up 12:31,  5 users,  load average: 2.19, 2.39, 1.77

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -39,10 +39,10 @@ static void os_init() {
   //To be continued:
   kmt->init();
   printf("%d\n\n\n\n\n",(int)_cpu());
-  _vme_init(pmm->alloc, pmm->free);
-  printf("before dev\n");
-  assert(0);
-  dev->init();
+//  _vme_init(pmm->alloc, pmm->free);
+  //printf("before dev\n");
+  //assert(0);
+  //dev->init();
   printf("after init");
   // 创建你的线程，线程可以调用`tty->ops->read`或`tty->ops->write`/
 }
