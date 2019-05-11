cfea80ce0093e1d8c3b2a1944eccf99c62243391
tracer-nju <tracer@nju.edu.cn>
2019-05-11 22:14:38 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 991e582a519a4bfe96e3a09ef409d730   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  22:14:38 up 12:46,  5 users,  load average: 2.87, 1.96, 1.78

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -8,7 +8,7 @@ spinlock_t free_lk;
 static spinlock head_lk;
 static uintptr_t pm_start, pm_end;
 static void pmm_init() {
-  spinlock*lk=&init_lk;
+  //spinlock*lk=&init_lk;
   //initlock(lk,NULL);
   //lock(lk);
 
