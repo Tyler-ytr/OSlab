26dacd6a015df5fd0fda750a93fcdcacb96f3852
tracer-nju <tracer@nju.edu.cn>
2019-05-12 22:46:14 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  22:46:14 up  9:18,  5 users,  load average: 0.57, 0.94, 1.10

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,7 +1,7 @@
 #include <kernel.h>
 #include <devices.h>
 #include <klib.h>
-//static void echo_task(void *arg);
+static void echo_task(void *arg);
 static void echo_task2(void *arg);
 int main() {
   _ioe_init();
@@ -9,7 +9,7 @@ int main() {
 
   // call sequential init code
   os->init();
-kmt->create(pmm->alloc(sizeof(task_t)), "print1", echo_task2, "tty1");
+kmt->create(pmm->alloc(sizeof(task_t)), "print1", echo_task, "tty1");
 kmt->create(pmm->alloc(sizeof(task_t)), "print2", echo_task2, "tty2");
 kmt->create(pmm->alloc(sizeof(task_t)), "print3", echo_task2, "tty3");
 //kmt->create(pmm->alloc(sizeof(task_t)), "print3", echo_task, "tty3");
@@ -18,7 +18,7 @@ kmt->create(pmm->alloc(sizeof(task_t)), "print3", echo_task2, "tty3");
 
   return 1;
 }
-/*
+
 static void echo_task(void *arg){
   char *name=(char*)arg;
   printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
@@ -36,7 +36,7 @@ static void echo_task(void *arg){
   printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
     tty->ops->write(tty,0,text,strlen(text));
   }
-}*/
+}
 static void echo_task2(void *arg){
   char *name=(char*)arg;
   printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
