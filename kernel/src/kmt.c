51cb1f1276c9d8b4997729dff998b1476972bb9e
tracer-nju <tracer@nju.edu.cn>
2019-05-12 15:40:53 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  15:40:53 up  2:12,  5 users,  load average: 0.67, 0.58, 0.70

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -119,6 +119,7 @@ static _Context *kmt_context_switch(_Event ev, _Context *context){
     }
 
   Log1("current_task[%d]: %s status:%d\n",(int)_cpu(),current_task[(int)_cpu()]->name,current_task[(int)_cpu()]->status);
+  Log1("head_task[%d]: %s status:%d\n",(int)_cpu(),task_head[(int)_cpu()]->name,task_head[(int)_cpu()]->status);
   }
   if(result==NULL){
     Log1("task_list_head[%d]->status %d\n",(int)_cpu(),task_head[(int)_cpu()]->status);
