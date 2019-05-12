5acdd8f69d9f0c73b5f62aa24a7dcfd2a02a2ae7
tracer-nju <tracer@nju.edu.cn>
2019-05-12 21:06:29 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  21:06:29 up  7:38,  5 users,  load average: 3.74, 2.97, 1.97

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,12 +1,28 @@
 #include <kernel.h>
 #include <klib.h>
-
+static void echo_task(void *arg);
 int main() {
   _ioe_init();
   _cte_init(os->trap);
 
   // call sequential init code
   os->init();
+kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
   _mpe_init(os->run); // all cores call os->run()
+
   return 1;
+}
+
+static void echo_task(void *arg){
+  char *name=(char*)arg;
+  char text[128]="",line[128]="";
+  device_t *tty=dev_lookup(name);
+  while(1){
+    sprintf(text,"(%s)$",name);
+    tty->ops->write(tty,0,text,strlen(text));
+    int nread=tty->ops->read(tty,0,line.sizeof(line));
+    line[nread-1]='\0';
+    sprintf(text,"Echo:%s.\n",line);
+    tty->ops->write(tty,0,text,strlen(text));
+  }
 }
