c50e83fe4d1ee6e34c25cc1312a1244093377bff
tracer-nju <tracer@nju.edu.cn>
2019-05-12 16:40:16 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  16:40:16 up  3:12,  5 users,  load average: 1.04, 1.15, 1.58

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -147,7 +147,7 @@ static _Context *kmt_context_switch(_Event ev, _Context *context){
         //printf("ererer");
       if(now!=NULL&&now->status==_runningable){
         now->status=_running;
-    current_task[(int)_cpu()]->status=_runningable; //如果这个cpu只有一个线程,那就让它跑把;
+    if(current_task[(int)_cpu()]->status==_running)current_task[(int)_cpu()]->status==_runningable; //如果这个cpu只有一个线程,那就让它跑把;
         current_task[(int)_cpu]=now;
         result=&current_task[(int)_cpu()]->context;
         success_hint=1;
