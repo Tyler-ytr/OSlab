32a10256970c18653c440fbac602bd5130e27968
tracer-nju <tracer@nju.edu.cn>
2019-05-12 22:28:42 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  22:28:42 up  9:00,  5 users,  load average: 1.40, 1.06, 1.05

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -28,6 +28,7 @@ static void echo_task(void *arg){
     tty->ops->write(tty,0,text,strlen(text));
   printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
     int nread=tty->ops->read(tty,0,line,sizeof(line));
+  printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
     line[nread-1]='\0';
     sprintf(text,"Echo:%s.\n",line);
   printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
