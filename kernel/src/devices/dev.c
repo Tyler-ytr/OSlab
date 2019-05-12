556a4887cebf68f4c4d2f5d7bbe0c291388d5e22
tracer-nju <tracer@nju.edu.cn>
2019-05-12 14:33:49 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  14:33:49 up  1:05,  5 users,  load average: 1.67, 1.77, 1.44

--- kernel/src/devices/dev.c
+++ kernel/src/devices/dev.c
@@ -51,6 +51,7 @@ static void dev_init() {
 
   kmt->create(pmm->alloc(sizeof(task_t)), "input-task", input_task, NULL);
   kmt->create(pmm->alloc(sizeof(task_t)), "tty-task", tty_task, NULL);
+  printf("here in dev init\n\n\n\n\n\n");
 }
 
 MODULE_DEF(dev) {
