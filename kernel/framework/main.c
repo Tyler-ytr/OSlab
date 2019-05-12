58af41d6ae63c08a5941a462bcbb5bb06de251f5
tracer-nju <tracer@nju.edu.cn>
2019-05-12 22:30:42 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: c288786cec264150b3de6ec50fe9cc78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  22:30:42 up  9:02,  5 users,  load average: 3.05, 1.65, 1.26

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -2,6 +2,7 @@
 #include <devices.h>
 #include <klib.h>
 static void echo_task(void *arg);
+static void echo_task2(void *arg);
 int main() {
   _ioe_init();
   _cte_init(os->trap);
@@ -9,7 +10,7 @@ int main() {
   // call sequential init code
   os->init();
 kmt->create(pmm->alloc(sizeof(task_t)), "print1", echo_task, "tty1");
-//kmt->create(pmm->alloc(sizeof(task_t)), "print2", echo_task, "tty2");
+kmt->create(pmm->alloc(sizeof(task_t)), "print2", echo_task2, "tty2");
 //kmt->create(pmm->alloc(sizeof(task_t)), "print3", echo_task, "tty3");
 //kmt->create(pmm->alloc(sizeof(task_t)), "print4", echo_task, "tty4");
   _mpe_init(os->run); // all cores call os->run()
@@ -18,6 +19,24 @@ kmt->create(pmm->alloc(sizeof(task_t)), "print1", echo_task, "tty1");
 }
 
 static void echo_task(void *arg){
+  char *name=(char*)arg;
+  printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
+  char text[128]="",line[128]="";
+  device_t *tty=dev_lookup(name);
+  while(1){
+    sprintf(text,"(%s)$",name);
+  printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
+    tty->ops->write(tty,0,text,strlen(text));
+  printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
+    int nread=tty->ops->read(tty,0,line,sizeof(line));
+  printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
+    line[nread-1]='\0';
+    sprintf(text,"Echo:%s.\n",line);
+  printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
+    tty->ops->write(tty,0,text,strlen(text));
+  }
+}
+static void echo_task2(void *arg){
   char *name=(char*)arg;
   printf("%d\n\n\n\n\n\n\n\n",(int)_cpu());
   char text[128]="",line[128]="";
