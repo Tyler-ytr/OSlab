dd984425015d2beaa2973abee2c409bcbab5bebf
tracer-nju <tracer@nju.edu.cn>
2019-05-13 10:48:55 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 63af4bd179f94675a50ebd49d0fc698b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  10:48:55 up 37 min,  7 users,  load average: 1.01, 0.75, 0.85

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -58,7 +58,7 @@ static _Context *kmt_context_save(_Event ev, _Context *context){
   kmt_spin_lock(&task_lock);
     //TRACE_ENTRY;
   //printf("in kmt_save\n");
-  if(current_task[(int)_cpu()]==NULL){printf("%d herer\n\n",(int)_cpu());
+  if(current_task[(int)_cpu()]==NULL){Log2("in %d, NULL!!!",(int)_cpu());
     /*task_t *now=task_head[(int)_cpu()];
     while(now->next!=NULL){
       now=now->next;
