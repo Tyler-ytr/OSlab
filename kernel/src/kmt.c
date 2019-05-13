cd4cc8daf05e56c2504a67bccd25ac6e0dfa8f59
tracer-nju <tracer@nju.edu.cn>
2019-05-13 10:38:48 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 63af4bd179f94675a50ebd49d0fc698b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  10:38:48 up 27 min,  7 users,  load average: 1.21, 1.06, 0.98

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -67,6 +67,7 @@ static _Context *kmt_context_save(_Event ev, _Context *context){
   }
   else{
   current_task[(int)_cpu()]->context=*context;
+  if( current_task[(int)_cpu()]->next!=NULL)
   Log2("in save: cpu:%d name:%s status:%d",(int)_cpu(), current_task[(int)_cpu()]->name, current_task[(int)_cpu()]->status);
   //if(current_task[(int)_cpu()]->status==_running)
   //current_task[(int)_cpu()]->status=_runningable;
