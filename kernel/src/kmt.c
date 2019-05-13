199c4ea7f1fa66f3e43991338c9d39d3058f71f1
tracer-nju <tracer@nju.edu.cn>
2019-05-13 11:52:00 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 63af4bd179f94675a50ebd49d0fc698b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-18-generic       Architecture: x86-64  11:52:00 up  1:40,  7 users,  load average: 0.82, 1.11, 1.39

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -597,6 +597,7 @@ static void kmt_sem_signal(sem_t *sem){
     assert(sem->value>0);//队列为空;
   }else
   {
+    sem->start%=sem->MAXSIZE;
     printf("in semi signal: semi->name:%s,task->name:%s status:%d\n",sem->name,sem->task_list[sem->start]->name,sem->task_list[sem->start]->status); 
      for(int i=sem->start%sem->MAXSIZE;i<sem->end;i=(i+1)%sem->MAXSIZE){
       printf("in semi-signal[%d] name:%s\n",i,sem->task_list[i]->name);
