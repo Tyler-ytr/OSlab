abaeefe59262973a0072e594c11503e611ddc595
tracer-nju <tracer@nju.edu.cn>
2019-05-28 15:21:38 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: a44939e9a3be42beaae7f39bf90c5fc2   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  15:21:38 up 12 min,  3 users,  load average: 1.06, 0.87, 0.51

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,3 +1,25 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+#include<stdlib.h>
+
+
+struct {
+
+
+;
+};
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
+
   return 0;
 }
