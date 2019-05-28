9ff3718268411d36df8f76cf4f555d809eea5d78
tracer-nju <tracer@nju.edu.cn>
2019-05-28 16:06:43 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: a44939e9a3be42beaae7f39bf90c5fc2   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  16:06:43 up 57 min,  3 users,  load average: 1.47, 1.02, 1.05

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -6,20 +6,43 @@
 #include <assert.h>
 #include <fcntl.h>
 #include<stdlib.h>
+#include <sys/stat.h> 
+
+// struct {
+//   int
 
 
+// }MBR;
 
 
 
 
 
 int main(int argc, char *argv[]) {
-
+struct stat file_stat;
   for(int i=0;i<argc;i++)
   {
     printf("argc %d : %s \n",i,argv[i]);
   }
 
+  void * start;
+  int fd;
+  fd=open(argv[1],O_RDONLY);
+  if(fd==-1){
+    perror("Can't find the file!");
+    return 0;
+  }
+  if(fstat(fd,&file_stat)==-1){
+    perror("Fstat error!");
+    return 0;
+  };
+  
+  start=mmap(0,file_stat.st_size ,PROT_READ,MAP_SHARED,fd,0);
+  if(start==(void*)-1){
+    perror("Mmap!");
+    return 0;
+  }
+  close(fd);
 
 
 
