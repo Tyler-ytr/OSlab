b8496b0f0f926083f37b3f4b6a209b535ad7f6ab
tracer-nju <tracer@nju.edu.cn>
2019-05-29 10:50:19 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: b4366c2cbc0b4e30be1de07f0d21e69c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  10:50:19 up 36 min,  1 user,  load average: 1.10, 1.24, 1.23

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -145,9 +145,9 @@ struct stat file_stat;
   while(1){
     if((uint8_t)(0x20)==test2[cnt].bit[11]){
       printf("test2:%p\n",(void*)&(test2[cnt].bit[0]));
-      printf(" row[8]:0x%x",test2[cnt].bit[0]);
-      printf(" row[9]:0x%x",test2[cnt].bit[1]);
-      printf(" row[10]:0x%x",test2[cnt].bit[2]);
+      printf(" row[8]:0x%x",test2[cnt].bit[8]);
+      printf(" row[9]:0x%x",test2[cnt].bit[9]);
+      printf(" row[10]:0x%x",test2[cnt].bit[10]);
       printf("\n");
       
 
