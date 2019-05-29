c307a086b3b2c4e0dbb11c2b1b49d8ef72c2d1d2
tracer-nju <tracer@nju.edu.cn>
2019-05-29 11:02:55 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: b4366c2cbc0b4e30be1de07f0d21e69c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  11:02:55 up 48 min,  1 user,  load average: 1.01, 1.29, 1.31

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -145,7 +145,6 @@ struct stat file_stat;
   cnt=0;
   test2=now;
   
-  printf(" end:%p",end);
   printf(" testttt:%p",(void *)(end-(void*)(&(test2[1].bit[0])-&(test2[0].bit[0]))));
   while(1){
     if((uint8_t)(0x20)==test2[cnt].bit[11]){
@@ -176,6 +175,7 @@ struct stat file_stat;
 
 
   printf("start:%p\n",start);
+  printf(" end:%p",end);
   printf("test2:%p\n",(void*)&(test2[0].bit[0]));
   printf("0x%x\n",my_mbr.sec_bit_num);
   printf("0x%02x\n",test1[0].sec_bit_num[0]);
