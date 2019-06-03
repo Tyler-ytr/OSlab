eb4c17d40930c3a874d3e90e1da9afe94b7dcd27
tracer-nju <tracer@nju.edu.cn>
2019-06-03 21:11:05 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 077d017ca43e4ead9cfce0f04a60f9e6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  21:11:05 up  1:29,  1 user,  load average: 1.10, 1.27, 1.22

---
+++ libkvdb/tests/main.c
@@ -0,0 +1,17 @@
+#include "kvdb.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+  kvdb_t db;
+  const char *key = "operating-systems";
+  char *value;
+
+  kvdb_open(&db, "a.db"); // BUG: should check for errors
+  kvdb_put(&db, key, "three-easy-pieces");
+  value = kvdb_get(&db, key);
+  kvdb_close(&db);
+  printf("[%s]: [%s]\n", key, value);
+  free(value);
+  return 0;
+}
