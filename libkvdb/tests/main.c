16051ef577794c1cd0f0bb6fa0cbe03704781c9a
tracer-nju <tracer@nju.edu.cn>
2019-06-04 14:14:12 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: d662c48bf26c44798d4e956d634ffa74   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  14:14:12 up 51 min,  1 user,  load average: 0.64, 0.92, 0.94

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void process_test(){
+//  kvdb_t db;
+// //  const char *key = "operating-systems";
+//   char *value;
+//   pid_t fpid;
+
+//   if((fpid=fork())<0){
+//     perror("GG in fork!");
+//     return 0;
+//   }
+//   else if(fpid==0){
+//     const char *key="child";
+//     kvdb_put(&db,key,"possiblity");
+//     return 0;
+
+//   }
+//   if(kvdb(&db,""))
+//   kvdb_open(&db, "process.db"); // BUG: should check for errors
+//   kvdb_put(&db, key, "three-easy-pieces");
+//   value = kvdb_get(&db, key);
+//   kvdb_close(&db);
+//   printf("[%s]: [%s]\n", key, value);
+//   free(value);
+
+
+;
+}
+void thread_test(){
+  
+  ;
+}
+
 int main() {
   kvdb_t db;
   const char *key = "operating-systems";
@@ -13,5 +45,13 @@ int main() {
   kvdb_close(&db);
   printf("[%s]: [%s]\n", key, value);
   free(value);
+
+  process_test();
+  thread_test();
+
+
+
+
+
   return 0;
 }
