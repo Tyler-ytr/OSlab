d5457ccdbdfabdca8e0bf974d4fa737bc4c3cd4e
tracer-nju <tracer@nju.edu.cn>
2019-06-04 16:34:30 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: d662c48bf26c44798d4e956d634ffa74   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  16:34:30 up  3:11,  1 user,  load average: 0.76, 1.05, 1.41

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -52,8 +52,6 @@ static void level1_error(const char *error){
 
 
 
-
-
 //kvdb_open打开filename数据库文件(例如filename指向"a.db")，并将信息保存到db中。如果文件不存在，则创建，如果文件存在，则在已有数据库的基础上进行操作。
 int kvdb_open_origin(kvdb_t *db, const char *filename)
 {//绝对不安全,请记得修改;
@@ -65,29 +63,15 @@ int kvdb_open_origin(kvdb_t *db, const char *filename)
 
   return 0;
 }
-int kvdb_open(kvdb_t *db, const char *filename){
-  pthread_mutex_init(&db->mutex_lock,NULL);
-
-  if(pthread_mutex_lock(&db->mutex_lock)!=0){
-    perror("error: mutex_lock in kvdb_open");
-    return -1;
-  }
-  int result=kvdb_open_origin(db,filename);
-  if(pthread_mutex_unlock(&db->mutex_lock)!=0){
-    perror("error: mutex_unlock in kvdb_open");
-    return -1;
-  }
-  return result;
-}
 
 
 //kvdb_close关闭数据库并释放相关资源。关闭后的kvdb_t将不再能执行put/get操作；但不影响其他打开的kvdb_t。
-int kvdb_close(kvdb_t *db){
+int kvdb_close_origin(kvdb_t *db){
   fclose(db->fp);
   return 0;
 }
 //kvdb_put建立key到value的映射，如果把db看成是一个std::map<std::string,std::string>，则相当于执行db[key] = value;。因此如果在kvdb_put执行之前db[key]已经有一个对应的字符串，它将被value覆盖。
-int kvdb_put(kvdb_t *db, const char *key, const char *value){
+int kvdb_put_origin(kvdb_t *db, const char *key, const char *value){
   fseek(db->fp,0,SEEK_END);//读写位置移动到文件尾
   fwrite(key,1,strlen(key),db->fp);
   fwrite("\n",1,1,db->fp);
@@ -98,7 +82,7 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
 }
 //kvdb_get获取key对应的value，相当于返回db[key]。返回的value是通过动态内存分配实现的(例如malloc或strdup分配的空间)，因此在使用完毕后需要调用free释放。
 //如果db不合法、内存分配失败或key不存在，则kvdb_get返回空指针。
-char *kvdb_get(kvdb_t *db, const char *key){
+char *kvdb_get_origin(kvdb_t *db, const char *key){
   //按照上面的不安全的存储,现在是 key,\n,value,\n;
   char *result=NULL;
   static char _key[1<<20],_value[1<<20];
@@ -122,9 +106,58 @@ char *kvdb_get(kvdb_t *db, const char *key){
 
 
 
+int kvdb_open(kvdb_t *db, const char *filename){
+  pthread_mutex_init(&db->mutex_lock,NULL);
+
+  if(pthread_mutex_lock(&db->mutex_lock)!=0){
+    perror("error: mutex_lock in kvdb_open");
+    return -1;
+  }
+  int result=kvdb_open_origin(db,filename);
+  if(pthread_mutex_unlock(&db->mutex_lock)!=0){
+    perror("error: mutex_unlock in kvdb_open");
+    return -1;
+  }
+  return result;
+}
+int kvdb_close(kvdb_t *db){
+  if(pthread_mutex_lock(&db->mutex_lock)!=0){
+    perror("error: mutex_lock in kvdb_close");
+    return -1;
+  }
+  int result=kvdb_close_origin(db);
+  if(pthread_mutex_unlock(&db->mutex_lock)!=0){
+    perror("error: mutex_unlock in kvdb_close");
+    return -1;
+  }
+  return result;
+}
 
 
+char *kvdb_get(kvdb_t *db, const char *key){
+if(pthread_mutex_lock(&db->mutex_lock)!=0){
+  perror("error: mutex_lock in kvdb_get");
+  return NULL;
+}
+char *result=kvdb_get_origin(db,key);
+if(pthread_mutex_unlock(&db->mutex_lock)!=0){
+  perror("error: mutex_unlock in kvdb_get");
+  return NULL;
+}
+  return result;
+}
 
+int kvdb_put(kvdb_t *db, const char *key, const char *value){
+  if(pthread_mutex_lock(&db->mutex_lock)!=0){
+    perror("error: mutex_lock in kvdb_put");
+    return -1;
+  }
+  int result=kvdb_put_origin(db,key,value);
+  if(pthread_mutex_unlock(&db->mutex_lock)!=0){
+    perror("error: mutex_unlock in kvdb_put");
+    return -1;
+  }
+return result;
 
-
+}
 
