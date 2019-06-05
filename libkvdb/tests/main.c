c6e94ad5869c658b7db3995a428f269ba8ead6d8
tracer-nju <tracer@nju.edu.cn>
2019-06-05 14:52:02 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 6d509f61a4754b9a9ac3c5a1b5c377e2   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  14:52:02 up 53 min,  1 user,  load average: 1.08, 0.78, 0.78

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -263,16 +263,16 @@ int pthread_test(){
 int main(){
     //test1((void *)100);
 
-    pid_t pid = fork();
-    if(pid == 0){
-        pthread_test();
-    }else{
-        pid_t ppid = fork();
-        if(ppid == 0)
-            pthread_test();
-        else
-            pthread_test();
-    }
+    // pid_t pid = fork();
+    // if(pid == 0){
+    //     pthread_test();
+    // }else{
+    //     pid_t ppid = fork();
+    //     if(ppid == 0)
+    //         pthread_test();
+    //     else
+    //         pthread_test();
+    // }
 
     //pthread_test(1);
 
