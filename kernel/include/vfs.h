8d9b71b0081494234321aef86afa25583831345f
tracer-nju <tracer@nju.edu.cn>
2019-06-11 22:21:35 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: b7a245731a5f495485bfa3796bcf635f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  22:21:36 up  7:36,  1 user,  load average: 0.85, 0.92, 1.03

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -13,6 +13,7 @@ struct inode {
   filesystem_t *fs;
  // inodeops_t *ops; // 在inode被创建时，由文件系统的实现赋值
                    // inode ops也是文件系统的一部分
+  int mode;       //文件格式;
   
 };
 // struct inodeops {
