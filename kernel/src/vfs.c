ac7ef297984e3e53bd2fa47e4e5b434d4569110e
tracer-nju <tracer@nju.edu.cn>
2019-07-02 23:37:03 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 23f0e915465a4be285e4fc728dfabe78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-24-generic       Architecture: x86-64  23:37:03 up  1:01,  1 user,  load average: 1.35, 1.04, 0.88

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -104,7 +104,7 @@ int get_name(char * path,int *name_len){
 static int vinode_lookup(char *path){
   //根据path从 blkfs里面获得而信息;并且更新vfs的inode;
 
-printf("here");
+printf("here\n");
 
 int len=strlen(path);
 vinode_t buf;//inode 缓冲区;
@@ -118,7 +118,7 @@ if(path[len-1]=='/'){
 // }else{
 //   index=lookup_cur(path,&flag,VFS_ROOT,&offset);
 // }
-printf("%c\n",path[0]);
+printf("%c\n",path[len-1]);
 int index=(path[0] == '/') ? lookup_root(path, &flag, &offset)
                              : lookup_cur(path, &flag, VFS_ROOT, &offset);
 if(flag==1){
