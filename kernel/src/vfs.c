4addd1fc42e1e6d70b0dd73c58534067e3c6177a
tracer-nju <tracer@nju.edu.cn>
2019-07-03 00:02:05 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 23f0e915465a4be285e4fc728dfabe78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-24-generic       Architecture: x86-64  00:02:05 up  1:26,  1 user,  load average: 2.23, 1.64, 1.26

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -553,8 +553,8 @@ extern void ext2_init(fs_t * fs,const char * name ,device_t* dev);
    int offset = sprintf(
        outbuf, "-----------------------------------------------------------\n");
 
-  // offset += sprintf(outbuf + offset,
-  //                   "      index       name                  path        \n");
+   offset += sprintf(outbuf + offset,
+                     "      index       name                  path        \n");
   // offset += sprintf(outbuf + offset, ">>   %4d         %12s          %s\n\n",
   //                   index, vinodes[index].name, vinodes[index].path);
   // for (int k = vinodes[index].child; k != -1; k = vinodes[k].next) {
