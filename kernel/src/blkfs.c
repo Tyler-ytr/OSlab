804c1360070b743fd9d7ef6a07bb966f1b0ddef0
tracer-nju <tracer@nju.edu.cn>
2019-07-04 10:44:15 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 0aba5f99f45941ff996526396cd7ae94   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-24-generic       Architecture: x86-64  10:44:15 up  1:42,  1 user,  load average: 1.32, 1.19, 1.10

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -407,6 +407,8 @@ int ext2_readdir(filesystem_t *fs,int rinode_idx,int kth,vinode_t * buf){
    printf("rinode: %d, kth: %d\n", rinode_idx, kth);
           printf("dir0 name:%s\n",ext2->dir[0].name,ext2->dir[0].inode);
           printf("dir1 name:%s\n",ext2->dir[1].name,ext2->dir[1].inode);
+          printf("ind.blocks:%d\n\n\n",ext2->ind.blocks);
+          
   for (int i = 0; i < ext2->ind.blocks; i++) {
     ext2_rd_dir(ext2, ext2->ind.block[i]);
     printf("%d: ",i);
@@ -419,7 +421,7 @@ int ext2_readdir(filesystem_t *fs,int rinode_idx,int kth,vinode_t * buf){
           buf->mode=ext2->dir[k].mode;
           printf("name:%s\n",buf->name);
           //buf->type= ext2->dir[k].file_type;
-          //return 1;
+          return 1;
         }}
     }
   }
