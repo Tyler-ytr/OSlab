60bead5ef7baad38005e49d83e7e3140272d808e
tracer-nju <tracer@nju.edu.cn>
2019-07-04 10:38:02 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 0aba5f99f45941ff996526396cd7ae94   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-24-generic       Architecture: x86-64  10:38:02 up  1:36,  1 user,  load average: 1.41, 1.12, 1.05

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -81,7 +81,7 @@ void ext2_init(fs_t * fs,const char * name ,device_t* dev){
 //  ext2->dir[0].file_type=ext2->dir[1].file_type=TYPE_DIR;
 
   strcpy(ext2->dir[0].name,".");
-  strcpy(ext2->dir[1].name,"..");
+ // strcpy(ext2->dir[1].name,"..");
   ext2_wr_dir(ext2,ext2->ind.block[0]);
 
 //  int init_file=ext2_create(ext2,ext2->current_dir,"Hello_world",TYPE_FILE|RD_ABLE|WR_ABLE);
