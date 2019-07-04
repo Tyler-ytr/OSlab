6acd15c6d743a3a9dc0f94730058da51d41f51ab
tracer-nju <tracer@nju.edu.cn>
2019-07-04 10:16:22 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 0aba5f99f45941ff996526396cd7ae94   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-24-generic       Architecture: x86-64  10:16:22 up  1:14,  1 user,  load average: 0.52, 0.95, 1.08

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -405,7 +405,7 @@ int ext2_readdir(filesystem_t *fs,int rinode_idx,int kth,vinode_t * buf){
   for (int i = 0; i < ext2->ind.blocks; i++) {
     ext2_rd_dir(ext2, ext2->ind.block[i]);
     for (int k = 0; k < DIR_AMUT; k++) {
-          printf("dir name:%s\n",dir[k].name);
+          printf("dir name:%s\n",ext2->dir[k].name);
       if (ext2->dir[k].inode)//存在才返回;
         if (++cnt == kth) {
           strcpy(buf->name, ext2->dir[k].name);
