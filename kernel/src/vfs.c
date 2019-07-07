437fba2381b95b218811e1f87425f4ae7f36c67d
tracer-nju <tracer@nju.edu.cn>
2019-07-07 15:42:57 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 1fd13bd56753489d8d0e78d575557a0c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  15:42:57 up 25 min,  1 user,  load average: 0.79, 1.16, 1.03

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -678,7 +678,7 @@ extern int ext2_remove(ext2_t* ext2,int index,char* name,int mode);
   int now_index=vinode_lookup(path);//得到当前的inode;
   get_father_dir(path,father_dir_offset);
   int index=vinode_lookup(tempbuff);//得到父亲节点的inode;
-  printf("now: %d, father: %d",now_index.index);
+  printf("now: %d, father: %d",now_index,index);
     return 0;
   };
   int vfs_create_file(const char* path){
