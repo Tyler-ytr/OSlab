abf6363ad02cb610516011f967b7509543d000f9
tracer-nju <tracer@nju.edu.cn>
2019-07-07 18:31:48 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 1fd13bd56753489d8d0e78d575557a0c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  18:31:49 up  3:14,  1 user,  load average: 1.64, 1.29, 1.15

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -67,7 +67,8 @@ void ext2_init(fs_t * fs,const char * name ,device_t* dev){
   ext2_rd_blockbitmap(ext2);
   ext2_rd_inodebitmap(ext2);
   for(int i=0;i<512;i++){
-    printf("%x",ext2->inodebitmapbuf[i]);
+    //printf("%x",ext2->inodebitmapbuf[i]);
+   ext2->inodebitmapbuf[i]=0;
   }
   printf("\n\n\n\n\n");
   ext2->ind.mode = TYPE_DIR | RD_ABLE | WR_ABLE;
