4009bfefa35c4267098ce88d1be56d6c199448af
tracer-nju <tracer@nju.edu.cn>
2019-07-08 22:13:33 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 3db2d12d6ffa471c975e6b0eb920fe81   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  22:13:33 up  2:49,  1 user,  load average: 1.16, 1.40, 1.55

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -165,7 +165,7 @@ static void *kalloc(size_t size) {
   assert(ret!=NULL);
   //unlock(a_lk);
   printf("In alloc size: %d\n",size);
- // used_memory_reality+=size;
+  used_memory+=size;
    //     printf("used memory:%d\n",used_memory_reality);
   kmt->spin_unlock(a_lk);
   return ret;
