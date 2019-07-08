196b62cb95de738e9e8157492120f2fd6f41ff35
tracer-nju <tracer@nju.edu.cn>
2019-07-08 22:00:43 +0800

    Static hostname: larryytr-ThinkPad-T570          Icon name: computer-laptop            Chassis: laptop         Machine ID: 80175dab02614f51a3e62d74fe973751            Boot ID: 3db2d12d6ffa471c975e6b0eb920fe81   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  22:00:44 up  2:36,  1 user,  load average: 1.51, 1.77, 1.64

--- kernel/src/procfs.c
+++ kernel/src/procfs.c
@@ -36,8 +36,8 @@ void procfs_info(){
   for(int i=0;i<proc_total;i++){
     printf("proc name: %s, shedule_time :%d cpu_num:%d \n",proc_list[i].name,proc_list[i].schedule_time,proc_list[i].cpu_num);
   }
-
-  printf("Total memory: %d used_memory: %d \n",total_memory,used_memory_info());
+uint64_t temp=used_memory_info();
+  printf("Total memory: %d used_memory:%d \n",total_memory,temp);
 return;
 }
 
